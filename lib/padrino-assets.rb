26d635c6087a24ecfc257434ef189345f1e1bced
Benjamin Bloch <cirex@gamesol.org>
2012-02-17 17:49:25 -0500

    Use Sprockets context_class so helpers are available to assets

--- lib/padrino-assets.rb
+++ lib/padrino-assets.rb
@@ -7,28 +7,30 @@ FileSet.glob_require('padrino-assets/**/*.rb', __FILE__)
 module Padrino
   module Assets
     class << self
-      ##
+      ###
       # Returns a list of paths Sprockets will use in order to find assets used by the project
       #
       # @return [Array]
       #   List of assets paths
       #
       # @example
-      #   Padrino::Assets.load_paths << Padrino.root('vendor', '**', 'assets')
+      #   Padrino::Assets.load_paths << Dir[Padrino.root('vendor', '**', 'assets')]
       #
       # @since 0.1.0
       # @api public
       def load_paths
-        @_load_paths ||= ['app/assets/**', 'lib/assets/**'].map do |file|
-          Dir[Padrino.root(file)]
+        @_load_paths ||= ['app/assets/**', 'lib/assets/**'].map do |directory|
+          Dir[Padrino.root(directory)]
         end.flatten
       end
 
-      ##
+      ###
       # Returns the configured Sprockets environment
       #
       # @return [Sprockets::Environment]
       #   Sprockets environment
+      #
+      # @see https://github.com/sstephenson/sprockets/blob/master/lib/sprockets/environment.rb
       #
       # @since 0.1.0
       # @api public
@@ -36,11 +38,13 @@ module Padrino
         @_environment
       end
 
-      ##
+      ###
       # Returns a compiled manifest of our assets
       #
       # @return [Sprockets::Manifest]
       #   Sprockets manifest
+      #
+      # @see https://github.com/sstephenson/sprockets/blob/master/lib/sprockets/manifest.rb
       #
       # @since 0.1.0
       # @api public
@@ -64,9 +68,10 @@ module Padrino
         Padrino.after_load do
           require 'sprockets'
 
-          @_environment = Sprockets::Environment.new(app.root) do |environment|
+          environment = Sprockets::Environment.new(Padrino.root) do |environment|
             environment.logger  = app.logger
             environment.version = app.assets_version
+
             if defined?(Padrino::Cache)
               if app.respond_to?(:caching) && app.caching?
                 environment.cache = app.cache
@@ -77,16 +82,22 @@ module Padrino
               environment.js_compressor  = app.js_compressor
               environment.css_compressor = app.css_compressor
             end
+
+            load_paths.flatten.each do |path|
+              environment.append_path(path)
+            end
           end
 
-          load_paths.each { |path| environment.append_path(path) }
+          environment.context_class.class_eval do
+            include Helpers
+          end
 
-          @_environment = environment.index if app.index_assets?
-          @_manifest = Sprockets::Manifest.new(environment, app.manifest_file)
+          @_environment = app.index_assets ? environment.index : environment
+          @_manifest = Sprockets::Manifest.new(@_environment, app.manifest_file)
         end
 
-        Padrino::Tasks.files << Dir[File.dirname(__FILE__) + '/tasks/**/*.{rake,rb}']
+        Padrino::Tasks.files << Dir[File.dirname(__FILE__) + '/tasks/**/*.rake']
       end
-    end
-  end
-end
+    end # self
+  end # Assets
+end # Padrino
